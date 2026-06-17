1f3a9ea2221b0fe3513b71e17d823b37d4d17429
Birane2 <gangueoumar075@gmail.com>
2026-06-17 11:05:53 -1200

    Premier commit Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TopHeader from "./components/header/TopHeader";
+
+function App() {
+  return (
+    <>
+      <TopHeader />
+      <h1>Mon projet Ecommerce</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/TopHeader.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch, FaRegHeart } from "react-icons/fa";
+import { TiShoppingCart } from "react-icons/ti";
+import Logo from "../../img/logo.png";
+import "./header.css";
+
+function TopHeader() {
+  return (
+    <div className="top_header">
+      <div className="container">
+        <Link className="logo" to="/">
+          <img src={Logo} alt="Logo" />
+        </Link>
+
+        <form className="search_box">
+          <input type="text" placeholder="Search products..." />
+          <button type="submit">
+            <FaSearch />
+          </button>
+        </form>
+
+        <div className="header_icons">
+          <div className="icon">
+            <FaRegHeart />
+            <span className="count">0</span>
+          </div>
+
+          <div className="icon">
+            <TiShoppingCart />
+            <span className="count">0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopHeader;
