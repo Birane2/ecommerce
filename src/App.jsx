45efea8425c04aa2ea07bdf11d59924ec9e55771
Birane2 <gangueoumar075@gmail.com>
2026-06-17 13:01:42 -1200

    Modification du projet + swiperslide

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TopHeader from "./components/header/TopHeader";
 import BtmHeader from "./components/header/BtmHeader";
+import Home from "./page/home/Home";
 
 function App() {
   return (
     <>
       <TopHeader />
       <BtmHeader />
+      <Home />
     </>
   );
 }
---
+++ src/page/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSlider from "../../components/HeroSlider";
+import "./home.css";
+
+function Home() {
+  return (
+    <>
+      <HeroSlider />
+    </>
+  );
+}
+
+export default Home;
